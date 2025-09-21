5317fb463401905417c2fc69bfda550b5452ba1b
HarukiIsono <haruki.isono861@gmail.com>
2025-09-21 23:55:44 +0900

    refactor launch configuration and update package paths in simple_navigation.launch.py

--- launch/simple_navigation.launch.py
+++ launch/simple_navigation.launch.py
@@ -7,48 +7,48 @@ from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    package_dir = get_package_share_directory('sim_py_01')
-    urdf = os.path.join(package_dir, 'urdf', 'wheel_robot_simple.urdf')
-    rviz = os.path.join(package_dir, 'rviz', 'wheel_robot_simple_nav2.rviz')
-    world = os.path.join(package_dir, 'world', 'maze.world')
-    map = os.path.join(package_dir, 'maps', 'map_01.yaml')
-    nav2_params = os.path.join(package_dir, 'config', 'navigation_params.yaml')
-    os.environ['GAZEBO_MODEL_PATH'] = os.path.join(package_dir, 'models')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    package_dir = get_package_share_directory('lucia_navigation2')
+    #urdf = os.path.join(package_dir, 'urdf', 'wheel_robot_simple.urdf')
+    rviz = os.path.join(package_dir, 'rviz', 'simple_nav2.rviz')
+    #world = os.path.join(package_dir, 'world', 'maze.world')
+    map = os.path.join(package_dir, 'map', 'map_e3.yaml')
+    nav2_params = os.path.join(package_dir, 'param', 'nav2_minimal.yaml')
+    #os.environ['GAZEBO_MODEL_PATH'] = os.path.join(package_dir, 'models')
     nav2_package_dir = get_package_share_directory('nav2_bringup')
     return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=[urdf],),
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=[urdf],),
+        # Node(
+        #     package='robot_state_publisher',
+        #     executable='robot_state_publisher',
+        #     name='robot_state_publisher',
+        #     output='screen',
+        #     parameters=[{'use_sim_time': use_sim_time}],
+        #     arguments=[urdf],),
+        # Node(
+        #     package='joint_state_publisher',
+        #     executable='joint_state_publisher',
+        #     name='joint_state_publisher',
+        #     parameters=[{'use_sim_time': use_sim_time}],
+        #     arguments=[urdf],),
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             arguments=['-d', rviz],),
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', '-s',
-                 'libgazebo_ros_init.so', '-s',
-                 'libgazebo_ros_factory.so', world],
-            output='screen',),
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            name='urdf_spawner',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['-topic', '/robot_description',
-                       '-entity', 'wheel_robot_simple'],),
-        SetRemap(src='cmd_vel', dst='/wheel_robot_simple/cmd_vel'),
-        SetRemap(src='odom', dst='/wheel_robot_simple/odom'),
+        # ExecuteProcess(
+        #     cmd=['gazebo', '--verbose', '-s',
+        #          'libgazebo_ros_init.so', '-s',
+        #          'libgazebo_ros_factory.so', world],
+        #     output='screen',),
+        # Node(
+        #     package='gazebo_ros',
+        #     executable='spawn_entity.py',
+        #     name='urdf_spawner',
+        #     parameters=[{'use_sim_time': use_sim_time}],
+        #     arguments=['-topic', '/robot_description',
+        #                '-entity', 'wheel_robot_simple'],),
+        # SetRemap(src='cmd_vel', dst='/wheel_robot_simple/cmd_vel'),
+        # SetRemap(src='odom', dst='/wheel_robot_simple/odom'),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(nav2_package_dir, 'launch',
