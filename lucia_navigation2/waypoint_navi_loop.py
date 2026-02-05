e47ea3c6bcd37c9a4928f360b2cefcde32566140
Haruki Isono <haruki.isono861@gmail.com>
2026-02-05 14:07:42 +0900

    fix

--- lucia_navigation2/waypoint_navi_loop.py
+++ lucia_navigation2/waypoint_navi_loop.py
@@ -31,17 +31,21 @@ class LoopRouteNavigator(Node):
         # Define route (waypoints + final goal)
         self.waypoints = [
             # make_pose(x, y, yaw)
-            make_pose(3.76, 1.07, 0.0),     # Waypoint 1
-            make_pose(3.76, -0.02, 1.57),   # Waypoint 2
-            make_pose(2.5, -1.4, 3.14),     # Waypoint 3
+            make_pose(2.06, -0.61, 0.0),     # Waypoint 1
+            make_pose(3.94, -1.38, 0.0),   # Waypoint 2
+            make_pose(4.49, -1.33, 0.0),     # Waypoint 3
+            make_pose(4.49, 0.13, 3.14),
+            make_pose(3.48, 2.59, 3.14),
+            make_pose(2.05, 1.52, 3.14),
+            make_pose(0.46, 0.85, 3.14),
         ]
         self.final_goal = make_pose(2.0, 2.0, 0.0)   # Final goal of the loop
 
     def run(self):
         # --- Set initial pose
-        initial_pose = make_pose(0.0, 0.0, 0.0)
-        self.get_logger().info('Setting initial pose...')
-        self.navigator.setInitialPose(initial_pose)
+        # initial_pose = make_pose(0.0, 0.0, 0.0)
+        # self.get_logger().info('Setting initial pose...')
+        # self.navigator.setInitialPose(initial_pose)
 
         # --- Wait for Nav2 to become active
         self.get_logger().info('Waiting for Nav2 to become active...')
