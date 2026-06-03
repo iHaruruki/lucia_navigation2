83c11fce10a9cf0063d12742a04327e03df76c1b
Haruki Isono <haruki.isono861@gmail.com>
2026-06-03 20:52:07 +0900

    wip

--- lucia_navigation2/waypoint_navi_loop.py
+++ lucia_navigation2/waypoint_navi_loop.py
@@ -31,12 +31,12 @@ class LoopRouteNavigator(Node):
         # Define route (waypoints + final goal)
         self.waypoints = [
             # make_pose(x, y, yaw)
-            make_pose(-3.41, -4.13, 0.0),    # Waypoint 1
-            make_pose(-4.5, -8.86, 0.0),    # Waypoint 2
-            make_pose(-0.17, -9.34, 0.0),    # Waypoint 3
-            make_pose(4.06, 8.50, 3.14),    # Waypoint 4
-            make_pose(2.95, -4.12, 3.14),    # Waypoint 5
-            make_pose(-0.65, 0.62, 3.14),    # Waypoint 6
+            make_pose(-5.87, 4.69, 2.72),    # Waypoint 1
+            make_pose(-9.73, 5.00, 2.57),    # Waypoint 2
+            make_pose(-10.97, 1.04, -1.58),    # Waypoint 3
+            make_pose(-10.71, -4.63, 0.36),    # Waypoint 4
+            make_pose(-6.25, -2.69, 0.42),    # Waypoint 5
+            make_pose(-0.49, 0.77, 2.60),    # Waypoint 6
         ]
         self.final_goal = make_pose(2.0, 2.0, 0.0)   # Final goal of the loop
 
